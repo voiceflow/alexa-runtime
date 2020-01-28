424b08905c143b5467de1ace718d111987d509b7
leartgjoni-voiceflow <leart@voiceflow.com>
2020-01-28 12:18:31 -0500

    feat: permission card and account linking handlers
    feat: user info handler (#11)
    
    * feat: user info handler
    
    * wip: added functionality
    
    * feat: finish logic
    
    * wip: cleanup
    
    * chore: use value instead of label
    
    * chore: cr fixes
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- lib/services/voiceflow/handlers/index.ts
+++ lib/services/voiceflow/handlers/index.ts
@@ -1,5 +1,8 @@
+import AccountLinkingHandler from './accountLinking';
 import CaptureHandler from './capture';
 import InteractionHandler from './interaction';
+import PermissionCardHandler from './permissionCard';
 import SpeakHandler from './speak';
+import UserInfoHandler from './userInfo';
 
-export default [SpeakHandler, InteractionHandler, CaptureHandler];
+export default [SpeakHandler, InteractionHandler, CaptureHandler, AccountLinkingHandler, PermissionCardHandler, UserInfoHandler];
