b2171b8bbbfc3a486b34da75e5f412e6f727ead5
leartgjoni-voiceflow <leart@voiceflow.com>
2020-01-28 13:59:32 -0500

    feat: user info handler

--- lib/services/voiceflow/handlers/index.ts
+++ lib/services/voiceflow/handlers/index.ts
@@ -1,7 +1,7 @@
-import AccountLinkingHandler from './accountLinkingHandler';
+import AccountLinkingHandler from './accountLinking';
 import CaptureHandler from './capture';
 import InteractionHandler from './interaction';
-import PermissionCardHandler from './permissionCardHandler';
+import PermissionCardHandler from './permissionCard';
 import SpeakHandler from './speak';
 
 export default [SpeakHandler, InteractionHandler, CaptureHandler, AccountLinkingHandler, PermissionCardHandler];
