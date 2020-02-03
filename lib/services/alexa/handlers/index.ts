c200a59ba42ce47700378bb3a041f5cdb62634aa
Tyler Han <tylerhan97@gmail.com>
2020-02-03 16:03:20 -0500

    fix: remove copy pasted audio player

--- lib/services/alexa/handlers/index.ts
+++ lib/services/alexa/handlers/index.ts
@@ -1,6 +1,5 @@
 export { default as IntentHandler } from './intent';
 export { default as LaunchHandler } from './launch';
-// export { default as AudioplayerHandler } from './audioplayer';
 export { default as ErrorHandler } from './error';
 export { default as PlaybackControllerHandler } from './playback';
 export { default as EventHandler } from './event';
