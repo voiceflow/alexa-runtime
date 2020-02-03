32933aefb35981a2fa8992325b3523915c6bedd1
leartgjoni-voiceflow <leart@voiceflow.com>
2020-02-03 14:00:14 -0500

    chore: enum

--- lib/services/alexa/handlers/purchase.ts
+++ lib/services/alexa/handlers/purchase.ts
@@ -5,10 +5,15 @@ import { S } from '@/lib/constants';
 import { updateContext } from '../utils';
 import IntentHandler from './intent';
 
+enum REQUEST {
+  RESPONSE_TYPE = 'Connections.Response',
+  REQ_NAME = 'Buy',
+}
+
 const PurchaseHandler: RequestHandler = {
   canHandle(input: HandlerInput): boolean {
     const { request } = input.requestEnvelope;
-    return request.type === 'Connections.Response' && request.name === 'Buy';
+    return request.type === REQUEST.RESPONSE_TYPE && request.name === REQUEST.REQ_NAME;
   },
   async handle(input: HandlerInput) {
     const { payload, status } = input.requestEnvelope.request as any;
