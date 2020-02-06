f3c93d28be9e315fb7b7dde9029e55c19f730fd9
leartgjoni-voiceflow <leart@voiceflow.com>
2020-02-06 12:20:28 -0500

    fix: routeWrapper 4

--- lib/middlewares/alexa.ts
+++ lib/middlewares/alexa.ts
@@ -1,12 +1,9 @@
 import verifier from 'alexa-verifier-middleware';
-import { NextFunction, Request, Response } from 'express';
 
 import { AbstractMiddleware } from './utils';
 
 class AlexaMiddleware extends AbstractMiddleware {
-  verifier(request: Request, response: Response, next: NextFunction) {
-    return verifier(request, response, next);
-  }
+  verifier = verifier;
 }
 
 export default AlexaMiddleware;
