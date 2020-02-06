808e6a5e0a70e7ac7a1ef10fa153c0ad708f71dc
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2020-02-06 17:00:11 -0500

    fix: payment handler (#26)
    * fix: payment handler
    
    * refactor: cleaner

--- lib/services/alexa/handlers/index.ts
+++ lib/services/alexa/handlers/index.ts
@@ -3,3 +3,4 @@ export { default as LaunchHandler } from './launch';
 export { default as ErrorHandler } from './error';
 export { default as PlaybackControllerHandler } from './playback';
 export { default as EventHandler } from './event';
+export { default as PurchaseHandler } from './purchase';
