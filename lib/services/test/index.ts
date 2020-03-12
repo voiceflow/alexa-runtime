021d54ef33bd5c4b771887ac3aba2abe7e1b989c
Tyler Han <tylerhan97@gmail.com>
2020-03-12 14:46:48 -0400

    chore: test endpoint

--- lib/services/test/index.ts
+++ lib/services/test/index.ts
@@ -9,7 +9,9 @@ class TestManager extends AbstractManager {
   async invoke(state: State) {
     const { voiceflow } = this.services;
 
-    const context = voiceflow.createContext(TEST_VERSION_ID, state as State);
+    const context = voiceflow.createContext(TEST_VERSION_ID, state as State, undefined, {
+      endpoint: `${this.config?.VF_DATA_ENDPOINT}/test`,
+    });
 
     context.setEvent(EventType.handlerWillHandle, (event) => addBlockTrace(event.context, event.block.blockID));
 
