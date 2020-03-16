a4bbe9c878efd17a5f1ace5cd1e719e984955270
Evgeny <overhawlin@gmail.com>
2020-03-16 19:19:39 +0300

    wip: add request to the test tool

--- lib/controllers/test.ts
+++ lib/controllers/test.ts
@@ -3,8 +3,8 @@ import { State } from '@voiceflow/client';
 import { AbstractController } from './utils';
 
 class TestController extends AbstractController {
-  handler = async (req: { body: State }, res: { json: (object: object) => void }) => {
-    const data = await this.services.test.invoke(req.body);
+  handler = async (req: { body: { state: State; request?: any } }, res: { json: (object: object) => void }) => {
+    const data = await this.services.test.invoke(req.body.state, req.body.request);
 
     res.json(data);
 
--- lib/services/test/index.ts
+++ lib/services/test/index.ts
@@ -1,19 +1,25 @@
 import { EventType, State } from '@voiceflow/client';
+import { IntentRequest as AlexaIntentRequest } from 'ask-sdk-model';
 
-import { TEST_VERSION_ID } from '@/lib/constants';
+import { T, TEST_VERSION_ID } from '@/lib/constants';
+import { RequestType } from '@/lib/services/voiceflow/types';
 
 import { AbstractManager } from '../utils';
 import { addBlockTrace } from './utils';
 
+type Request = { type: RequestType; payload: AlexaIntentRequest };
+
 class TestManager extends AbstractManager {
-  async invoke(state: State) {
+  async invoke(state: State, request?: Request) {
     const { voiceflow } = this.services;
 
-    const context = voiceflow.createContext(TEST_VERSION_ID, state as State, undefined, {
+    const context = voiceflow.createContext(TEST_VERSION_ID, state as State, request, {
       endpoint: `${this.config?.VF_DATA_ENDPOINT}/test`,
     });
 
     context.setEvent(EventType.handlerWillHandle, (event) => addBlockTrace(event.context, event.block.blockID));
+
+    context.turn.set(T.REQUEST, request);
 
     await context.update();
 
