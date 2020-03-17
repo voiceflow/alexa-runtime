2fac43aaf3c3451afd381a7760f1e2af6495dc32
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-17 09:57:43 -0400

    refactor: controller handler to method

--- lib/controllers/test.ts
+++ lib/controllers/test.ts
@@ -3,13 +3,9 @@ import { State } from '@voiceflow/client';
 import { AbstractController } from './utils';
 
 class TestController extends AbstractController {
-  handler = async (req: { body: { state: State; request?: any } }, res: { json: (object: object) => void }) => {
-    const data = await this.services.test.invoke(req.body.state, req.body.request);
-
-    res.json(data);
-
-    return data;
-  };
+  async handler(req: { body: { state: State; request?: any } }) {
+    return this.services.test.invoke(req.body.state, req.body.request);
+  }
 }
 
 export default TestController;
