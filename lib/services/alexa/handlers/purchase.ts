61765d2bef1de05298413c030af94f046af5aa7c
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-19 12:27:47 -0400

    test: purchase handler unit

--- lib/services/alexa/handlers/purchase.ts
+++ lib/services/alexa/handlers/purchase.ts
@@ -6,12 +6,17 @@ import { S } from '@/lib/constants';
 import { updateContext } from '../utils';
 import IntentHandler from './intent';
 
-enum Request {
+export enum Request {
   RESPONSE_TYPE = 'Connections.Response',
   REQ_NAME = 'Buy',
 }
 
-const PurchaseHandler: RequestHandler = {
+const utilsObj = {
+  IntentHandler,
+  updateContext,
+};
+
+export const PurchaseHandlerGenerator = (utils: typeof utilsObj): RequestHandler => ({
   canHandle(input: HandlerInput): boolean {
     const { request } = input.requestEnvelope;
 
@@ -21,14 +26,14 @@ const PurchaseHandler: RequestHandler = {
     const { payload, status } = input.requestEnvelope.request as interfaces.connections.ConnectionsResponse;
     const result: false | undefined | interfaces.monetization.v1.PurchaseResult = +(status?.code || 0) < 300 && payload?.purchaseResult;
 
-    await updateContext(input, (context) => {
+    await utils.updateContext(input, (context) => {
       context.storage.produce((draft) => {
         if (draft[S.PAYMENT]) draft[S.PAYMENT].status = result || false;
       });
     });
 
-    return IntentHandler.handle(input);
+    return utils.IntentHandler.handle(input);
   },
-};
+});
 
-export default PurchaseHandler;
+export default PurchaseHandlerGenerator(utilsObj);
---
+++ tests/lib/services/alexa/handlers/purchase.unit.ts
@@ -0,0 +1,125 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { S } from '@/lib/constants';
+import PurchaseHandler, { PurchaseHandlerGenerator, Request } from '@/lib/services/alexa/handlers/purchase';
+
+describe('launch handler unit tests', () => {
+  describe('canHandle', () => {
+    it('false', () => {
+      expect(PurchaseHandler.canHandle({ requestEnvelope: { request: { type: 'random-type', name: 'random-name' } } } as any)).to.eql(false);
+    });
+
+    it('true', () => {
+      expect(PurchaseHandler.canHandle({ requestEnvelope: { request: { type: Request.RESPONSE_TYPE, name: Request.REQ_NAME } } } as any)).to.eql(
+        true
+      );
+    });
+  });
+
+  describe('handle', () => {
+    it('works correctly', async () => {
+      const output = 'output';
+
+      const utils = {
+        IntentHandler: { handle: sinon.stub().returns(output) },
+        updateContext: sinon.stub(),
+      };
+
+      const handler = PurchaseHandlerGenerator(utils as any);
+
+      const purchaseResult = { foo: 'bar' };
+      const input = { requestEnvelope: { request: { status: { code: 200 }, payload: { purchaseResult } } } };
+      expect(await handler.handle(input as any)).to.eql(output);
+      expect(utils.IntentHandler.handle.args).to.eql([[input]]);
+      expect(utils.updateContext.args[0][0]).to.eql(input);
+      // assert updateContext callback
+      const fn = utils.updateContext.args[0][1];
+      const context = {
+        storage: { produce: sinon.stub() },
+      };
+      fn(context);
+
+      const fn2 = context.storage.produce.args[0][0];
+      const draft = { [S.PAYMENT]: { status: null } };
+      fn2(draft);
+      expect(draft[S.PAYMENT]).to.eql({ status: purchaseResult });
+    });
+
+    it('no status', async () => {
+      const output = 'output';
+
+      const utils = {
+        IntentHandler: { handle: sinon.stub().returns(output) },
+        updateContext: sinon.stub(),
+      };
+
+      const handler = PurchaseHandlerGenerator(utils as any);
+
+      const purchaseResult = { foo: 'bar' };
+      const input = { requestEnvelope: { request: { payload: { purchaseResult } } } };
+      expect(await handler.handle(input as any)).to.eql(output);
+      // assert updateContext callback
+      const fn = utils.updateContext.args[0][1];
+      const context = {
+        storage: { produce: sinon.stub() },
+      };
+      fn(context);
+
+      const fn2 = context.storage.produce.args[0][0];
+      const draft = { [S.PAYMENT]: { status: null } };
+      fn2(draft);
+      expect(draft[S.PAYMENT]).to.eql({ status: purchaseResult });
+    });
+
+    it('no payload', async () => {
+      const output = 'output';
+
+      const utils = {
+        IntentHandler: { handle: sinon.stub().returns(output) },
+        updateContext: sinon.stub(),
+      };
+
+      const handler = PurchaseHandlerGenerator(utils as any);
+
+      const input = { requestEnvelope: { request: { status: { code: 200 } } } };
+      expect(await handler.handle(input as any)).to.eql(output);
+      // assert updateContext callback
+      const fn = utils.updateContext.args[0][1];
+      const context = {
+        storage: { produce: sinon.stub() },
+      };
+      fn(context);
+
+      const fn2 = context.storage.produce.args[0][0];
+      const draft = { [S.PAYMENT]: { status: null } };
+      fn2(draft);
+      expect(draft[S.PAYMENT]).to.eql({ status: false });
+    });
+
+    it('no storage payment', async () => {
+      const output = 'output';
+
+      const utils = {
+        IntentHandler: { handle: sinon.stub().returns(output) },
+        updateContext: sinon.stub(),
+      };
+
+      const handler = PurchaseHandlerGenerator(utils as any);
+
+      const input = { requestEnvelope: { request: {} } };
+      expect(await handler.handle(input as any)).to.eql(output);
+      // assert updateContext callback
+      const fn = utils.updateContext.args[0][1];
+      const context = {
+        storage: { produce: sinon.stub() },
+      };
+      fn(context);
+
+      const fn2 = context.storage.produce.args[0][0];
+      const draft = { foo: 'bar' };
+      fn2(draft);
+      expect(draft).to.eql({ foo: 'bar' });
+    });
+  });
+});
