67303567f67ecea3fe793a6e7a2e602e6402b2e6
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-19 12:00:40 -0400

    test: launch handler unit

---
+++ tests/lib/services/alexa/handlers/launch.unit.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import LaunchHandler, { LaunchHandlerGenerator, Request } from '@/lib/services/alexa/handlers/launch';
+
+describe('launch handler unit tests', () => {
+  describe('canHandle', () => {
+    it('false', () => {
+      expect(LaunchHandler.canHandle({ requestEnvelope: { request: { type: 'random' } } } as any)).to.eql(false);
+    });
+
+    it('true', () => {
+      expect(LaunchHandler.canHandle({ requestEnvelope: { request: { type: Request.LAUNCH } } } as any)).to.eql(true);
+      expect(LaunchHandler.canHandle({ requestEnvelope: { request: { type: Request.CAN_FULFILL_INTENT } } } as any)).to.eql(true);
+    });
+  });
+
+  describe('handle', () => {
+    it('works correclty', async () => {
+      const output = 'output';
+
+      const context = { foo: 'bar' };
+
+      const utils = {
+        buildContext: sinon.stub().returns(context),
+        initialize: sinon.stub(),
+        update: sinon.stub(),
+        buildResponse: sinon.stub().returns(output),
+      };
+
+      const handler = LaunchHandlerGenerator(utils);
+
+      const input = { input: 'obj' };
+
+      expect(await handler.handle(input as any)).to.eql(output);
+      expect(utils.buildContext.args).to.eql([[input]]);
+      expect(utils.initialize.args).to.eql([[context, input]]);
+      expect(utils.update.args).to.eql([[context]]);
+      expect(utils.buildResponse.args).to.eql([[context, input]]);
+    });
+  });
+});
