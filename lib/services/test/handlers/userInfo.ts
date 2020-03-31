edd81b2e67ce2760a67faa4bb5748f95800ccf25
Evgeny <overhawlin@gmail.com>
2020-03-31 20:04:19 +0300

    feat: test tool happy paths

---
+++ lib/services/test/handlers/userInfo.ts
@@ -0,0 +1,22 @@
+import { Handler } from '@voiceflow/client';
+
+import { UserInfo } from '@/lib/services/voiceflow/handlers/userInfo';
+
+const UserInfoHandlerGenerator: Handler<UserInfo> = {
+  canHandle: (block) => {
+    return !!block.permissions;
+  },
+  handle: async (block, context) => {
+    context.trace.debug('__User Info__ - entered');
+
+    if (block.success_id || block.fail_id) {
+      context.trace.debug(
+        block.success_id ? 'User Info - redirecting to the success block' : 'User Info - success link is not provided, redirecting to the fail block'
+      );
+    }
+
+    return block.success_id ?? block.fail_id ?? null;
+  },
+};
+
+export default UserInfoHandlerGenerator;
