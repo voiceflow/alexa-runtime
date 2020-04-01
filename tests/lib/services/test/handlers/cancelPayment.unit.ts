c716d68094152f48a798a7dea54d9553d5b6166f
leartgjoni-voiceflow <leart@voiceflow.com>
2020-04-01 14:26:49 -0400

    test: test tool cancelPayment handler unit

---
+++ tests/lib/services/test/handlers/cancelPayment.unit.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import CancelPaymentHandler from '@/lib/services/test/handlers/cancelPayment';
+
+describe('Test CancelPaymentHandler unit tests', () => {
+  describe('canHandle', () => {
+    it('false', () => {
+      expect(CancelPaymentHandler.canHandle({} as any, null as any, null as any, null as any)).to.eql(false);
+    });
+
+    it('true', () => {
+      expect(CancelPaymentHandler.canHandle({ cancel_product_id: '1' } as any, null as any, null as any, null as any)).to.eql(true);
+    });
+  });
+
+  describe('handle', () => {
+    it('no success_id or fail_id', () => {
+      const context = { trace: { debug: sinon.stub() } };
+      expect(CancelPaymentHandler.handle({} as any, context as any, null as any, null as any)).to.eql(null);
+      expect(context.trace.debug.args).to.eql([['__Cancel payment__ - entered']]);
+    });
+
+    it('success_id', () => {
+      const block = { success_id: 'success-id' };
+      const context = { trace: { debug: sinon.stub() } };
+      expect(CancelPaymentHandler.handle(block as any, context as any, null as any, null as any)).to.eql(block.success_id);
+      expect(context.trace.debug.args).to.eql([['__Cancel payment__ - entered'], ['__Cancel payment__ - redirecting to the success block']]);
+    });
+
+    it('fail_id', () => {
+      const block = { fail_id: 'fail-id' };
+      const context = { trace: { debug: sinon.stub() } };
+      expect(CancelPaymentHandler.handle(block as any, context as any, null as any, null as any)).to.eql(block.fail_id);
+      expect(context.trace.debug.args).to.eql([
+        ['__Cancel payment__ - entered'],
+        ['__Cancel payment__ - success link is not provided, redirecting to the fail block'],
+      ]);
+    });
+  });
+});
