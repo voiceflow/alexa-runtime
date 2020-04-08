ed1d8c7f37d2da50138af2dc1689906927f76742
leartgjoni-voiceflow <leart@voiceflow.com>
2020-04-08 11:27:24 -0400

    refactor: alexa verifier middleware

--- lib/middlewares/alexa.ts
+++ lib/middlewares/alexa.ts
@@ -1,9 +1,18 @@
-import verifier from 'alexa-verifier-middleware';
+import AlexaVerifier from 'alexa-verifier-middleware';
 
+import { Config } from '@/types';
+
+import { FullServiceMap } from '../services';
 import { AbstractMiddleware } from './utils';
 
 class AlexaMiddleware extends AbstractMiddleware {
-  verifier = process.env.NODE_ENV === 'test' ? (_: any, __: any, next: () => void) => next() : verifier;
+  public verifier: any;
+
+  constructor(public services: FullServiceMap, public config: Config) {
+    super(services, config);
+
+    this.verifier = this.config.NODE_ENV === 'test' ? (_: any, __: any, next: () => void) => next() : AlexaVerifier;
+  }
 }
 
 export default AlexaMiddleware;
