60733b0ae1cb9437a05aee3824d91d2f96cb7d9e
leartgjoni-voiceflow <leart@voiceflow.com>
2020-05-08 15:18:16 -0400

    test: new tests

--- tests/lib/services/adapter/index.unit.ts
+++ tests/lib/services/adapter/index.unit.ts
@@ -1,23 +1,62 @@
 import { expect } from 'chai';
+import _ from 'lodash';
 import sinon from 'sinon';
 
 import AdapterManager from '@/lib/services/adapter';
 
-import { newInteraction, oldInteraction } from './fixtures';
+import { newInteraction, newMalformed, oldInteraction, oldMalformed } from './fixtures';
 
 describe('adapterManager unit tests', async () => {
   afterEach(() => sinon.restore());
 
   describe('context', () => {
-    const tests = [{ text: 'wait on interaction', old: oldInteraction, new: newInteraction }];
+    it('empty', async () => {
+      const adapter = new AdapterManager(null as any, null as any);
+      const input = { attributesManager: { getPersistentAttributes: sinon.stub().resolves({}) } };
+
+      await adapter.context(input as any);
+      expect(input.attributesManager.getPersistentAttributes.callCount).to.eql(1);
+    });
+
+    it('new format', async () => {
+      const adapter = new AdapterManager(null as any, null as any);
+      const input = { attributesManager: { getPersistentAttributes: sinon.stub().resolves({ stack: [] }) } };
+
+      await adapter.context(input as any);
+      expect(input.attributesManager.getPersistentAttributes.callCount).to.eql(1);
+    });
+
+    it('old format', async () => {
+      const oldContext = { diagrams: [] };
+      const newContext = { stack: [] };
+      const transformContextStub = sinon.stub().resolves(newContext);
+
+      const adapter = new AdapterManager(null as any, null as any);
+      adapter.transformContext = transformContextStub;
+
+      const input = { attributesManager: { getPersistentAttributes: sinon.stub().resolves(oldContext), setPersistentAttributes: sinon.stub() } };
+
+      await adapter.context(input as any);
+      expect(input.attributesManager.getPersistentAttributes.callCount).to.eql(1);
+      expect(transformContextStub.args).to.eql([[oldContext, input]]);
+      expect(input.attributesManager.setPersistentAttributes.args).to.eql([[newContext]]);
+    });
+  });
+
+  describe('transformContext', () => {
+    const tests = [
+      { text: 'malformed', old: oldMalformed, new: newMalformed },
+      { text: 'wait on interaction', old: oldInteraction, new: newInteraction },
+    ];
 
     tests.forEach((test) => {
       it(test.text, async () => {
         const adapter = new AdapterManager(null as any, null as any);
 
-        expect(
-          await adapter.transformContext(test.old as any, { requestEnvelope: { context: { System: test.new.variables._system } } } as any)
-        ).to.deep.equalInAnyOrder(test.new);
+        const System = _.get(test.new, 'variables._system');
+        const input = { requestEnvelope: { context: { System } } };
+
+        expect(await adapter.transformContext(test.old as any, input as any)).to.deep.equalInAnyOrder(test.new);
       });
     });
   });
