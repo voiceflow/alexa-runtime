d3cfe6e7c4051c9c263921bb63222d5e9912a7df
leartgjoni-voiceflow <leart@voiceflow.com>
2020-05-13 12:15:53 -0400

    wip: add full speak capabitilies

--- lib/services/voiceflow/handlers/noMatch.ts
+++ lib/services/voiceflow/handlers/noMatch.ts
@@ -2,6 +2,8 @@ import { Context, Store } from '@voiceflow/client';
 
 import { S } from '@/lib/constants';
 
+import { regexVariables, sanitizeVariables } from '../utils';
+
 type Block = {
   blockID: string;
   noMatches?: string[];
@@ -11,17 +13,21 @@ export const NoMatchHandler = () => ({
   canHandle: (block: Block, context: Context) => {
     return (block.noMatches?.length ?? 0) > (context.storage.get(S.NO_MATCHES_COUNTER) ?? 0);
   },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handle: (block: Block, context: Context, _variables: Store) => {
+  handle: (block: Block, context: Context, variables: Store) => {
     context.storage.produce((draft) => {
       draft[S.NO_MATCHES_COUNTER] = draft[S.NO_MATCHES_COUNTER] ? draft[S.NO_MATCHES_COUNTER] + 1 : 1;
     });
 
-    const output = block.noMatches?.[context.storage.get(S.NO_MATCHES_COUNTER) - 1] || '';
+    const speak = block.noMatches?.[context.storage.get(S.NO_MATCHES_COUNTER) - 1] || '';
+
+    // turn float variables to 2 decimal places
+    const sanitizedVars = sanitizeVariables(variables.getState());
+    // replaces var values
+    const output = regexVariables(speak, sanitizedVars);
+
     context.storage.produce((draft) => {
       draft[S.OUTPUT] += output;
     });
-
     context.trace.speak(output);
 
     return block.blockID;
