221bcab5621fc56988758d215a57e2aaf1c93b1f
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2020-06-01 11:26:58 -0400

    feat: context adapter [CORE-2943] (#54)
    * wip: add adapter
    
    * wip: initial boilerplate
    
    * wip: more storage fields
    
    * wip: system variable
    
    * test: deep equal in any order
    
    * wip: access token storage key
    
    * wip: permissions storage
    
    * wip: payment storage
    
    * chore: flag
    
    * wip: cancel payment storage
    
    * chore: remove unused flag
    
    * chore: remove end from commands
    
    * wip: display info storage flag
    
    * wip: stream play storage flag
    
    * wip: stream pause storage flag
    
    * wip: stream finished storage flag
    
    * wip: stream temp storage flag
    
    * refactor: nicer code
    
    * chore: async
    
    * test: new tests
    
    * test: added fixtures
    
    * test: more fixtures
    
    * test: final fixtures

---
+++ lib/services/adapter/index.ts
@@ -0,0 +1,48 @@
+import { HandlerInput } from 'ask-sdk';
+import _ from 'lodash';
+
+import { AbstractManager } from '../utils';
+import { NewContextRaw, OldContextRaw } from './types';
+import { afterStorageModifier, beforeContextModifier, stackAdapter, storageAdapter, variablesAdapter } from './utils';
+
+/**
+ * Adapter to transform old vf-server sessions into the new format
+ * The intention is to remove this adapter once we switch all users over
+ */
+class AdapterManager extends AbstractManager {
+  async context(input: HandlerInput) {
+    // getPersistentAttributes hits dynamo only once during a TURN. the results from dynamo are cached
+    // and used for sequent calls to getPersistentAttributes
+    const context = await input.attributesManager.getPersistentAttributes();
+    if (!_.isEmpty(context) && !context.stack) {
+      const transformedContext = await this.transformContext(context as OldContextRaw, input);
+
+      // set transformed context
+      input.attributesManager.setPersistentAttributes(transformedContext);
+    }
+  }
+
+  async transformContext(context: OldContextRaw, input: HandlerInput): Promise<NewContextRaw | {}> {
+    try {
+      context = beforeContextModifier(context);
+
+      const stack = stackAdapter(context);
+      const variables = variablesAdapter(context, { system: input.requestEnvelope.context.System });
+      let storage = storageAdapter(context, { accessToken: input.requestEnvelope.context.System.user.accessToken });
+
+      storage = afterStorageModifier(storage, variables);
+
+      return {
+        stack,
+        storage,
+        variables,
+      };
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log('context adapter err: ', err.message);
+      return {};
+    }
+  }
+}
+
+export default AdapterManager;
