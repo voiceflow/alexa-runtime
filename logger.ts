c2066ba372039736abd15d31da5647adaee96fd0
(Frank) Yu Cheng Gu <frank@voiceflow.com>
2020-07-17 15:15:14 -0400

    fix: Frank/refactor log/core 0000 (#71)
    * fix: change logging middleware
    
    * fix: ci orb version
    
    * fix: bump logger verison

--- logger.ts
+++ logger.ts
@@ -1,6 +1,12 @@
 import Logger from '@voiceflow/logger';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Level } from 'pino'; // We are only using the types here
 
-const options = ['local', 'test'].includes(process.env.NODE_ENV || '') ? { level: 'info', stackTrace: true, pretty: true } : {};
+const infoLevel: Level = 'info';
+const warnLevel: Level = 'warn';
+const options = ['local', 'test'].includes(process.env.NODE_ENV || '')
+  ? { level: infoLevel, stackTrace: true, pretty: true }
+  : { level: warnLevel, stackTrace: true, pretty: false };
 
 const log = new Logger(options);
 
