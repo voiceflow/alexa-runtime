98bd1a0a06462b0c2bd47b1c9dbb308db5060906
Frank Gu <frank@voiceflow.com>
2020-08-12 13:11:20 -0400

    chore: reduce middleware log verbosity

--- logger.ts
+++ logger.ts
@@ -5,8 +5,8 @@ import { Level } from 'pino'; // We are only using the types here
 const infoLevel: Level = 'info';
 const warnLevel: Level = 'warn';
 const options = ['local', 'test'].includes(process.env.NODE_ENV || '')
-  ? { level: infoLevel, stackTrace: true, pretty: true }
-  : { level: warnLevel, stackTrace: true, pretty: false };
+  ? { level: infoLevel, stackTrace: true, pretty: true, middlewareVerbosity: 'short' }
+  : { level: warnLevel, stackTrace: true, pretty: false, middlewareVerbosity: 'full' };
 
 const log = new Logger(options);
 
