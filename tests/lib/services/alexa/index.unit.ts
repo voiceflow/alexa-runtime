43c709b392b69d76be356918fda3dd68b05f4796
Tyler Han <tylerhan97@gmail.com>
2020-09-03 17:03:39 -0400

    fix: versions

--- tests/lib/services/alexa/index.unit.ts
+++ tests/lib/services/alexa/index.unit.ts
@@ -76,6 +76,70 @@ describe('alexa manager unit tests', () => {
       expect(utils.handlers.ErrorHandlerGenerator.args[0]).to.eql([services.metrics]);
       expect(utils.interceptors.RequestInterceptorGenerator.args[0]).to.eql([services.metrics, services.adapter]);
     });
+
+    it('builds local skill correctly', () => {
+      const output = 'output';
+      const create = sinon.stub().returns(output);
+      const withPersistenceAdapter = sinon.stub().returns({ create });
+      const addResponseInterceptors = sinon.stub().returns({ withPersistenceAdapter });
+      const addRequestInterceptors = sinon.stub().returns({ addResponseInterceptors });
+      const addErrorHandlers = sinon.stub().returns({ addRequestInterceptors });
+      const addRequestHandlers = sinon.stub().returns({ addErrorHandlers });
+      const services = {
+        dynamo: 'dynamo',
+        metrics: 'metrics',
+        adapter: 'adapter',
+      };
+      const utils = {
+        handlers: {
+          EventHandler: 'EventHandler',
+          LaunchHandler: 'LaunchHandler',
+          IntentHandler: 'IntentHandler',
+          SessionEndedHandler: 'SessionEndedHandler',
+          PlaybackControllerHandler: 'PlaybackControllerHandler',
+          AudioPlayerEventHandler: 'AudioPlayerEventHandler',
+          PermissionHandler: 'PermissionHandler',
+          PurchaseHandler: 'PurchaseHandler',
+          APLUserEventHandler: 'APLUserEventHandler',
+          CancelPurchaseHandler: 'CancelPurchaseHandler',
+          ErrorHandlerGenerator: sinon.stub().returns('ErrorHandler'),
+        },
+        interceptors: { ResponseInterceptor: 'ResponseInterceptor', RequestInterceptorGenerator: sinon.stub().returns('RequestInterceptor') },
+        builder: { custom: sinon.stub().returns({ addRequestHandlers }) },
+        adapters: {
+          MemoryPersistenceAdapter: sinon.stub().returns({ foo: 'bar' }),
+        },
+      };
+      const config = { SESSIONS_DYNAMO_TABLE: 'SESSIONS_DYNAMO_TABLE', SESSIONS_SOURCE: 'local' };
+
+      const alexaManager = AlexaManager(services as any, config as any, utils as any);
+
+      expect(alexaManager.skill).to.eql(output);
+      expect(utils.builder.custom.callCount).to.eql(1);
+      expect(addRequestHandlers.args).to.eql([
+        [
+          utils.handlers.EventHandler,
+          utils.handlers.LaunchHandler,
+          utils.handlers.IntentHandler,
+          utils.handlers.SessionEndedHandler,
+          utils.handlers.PlaybackControllerHandler,
+          utils.handlers.AudioPlayerEventHandler,
+          utils.handlers.PermissionHandler,
+          utils.handlers.PurchaseHandler,
+          utils.handlers.APLUserEventHandler,
+          utils.handlers.CancelPurchaseHandler,
+        ],
+      ]);
+      expect(addErrorHandlers.args).to.eql([[utils.handlers.ErrorHandlerGenerator()]]);
+      expect(addRequestInterceptors.args).to.eql([[utils.interceptors.RequestInterceptorGenerator()]]);
+      expect(addResponseInterceptors.args).to.eql([[utils.interceptors.ResponseInterceptor]]);
+      expect(withPersistenceAdapter.args).to.eql([[{ foo: 'bar' }]]);
+      expect(create.callCount).to.eql(1);
+      expect(utils.adapters.MemoryPersistenceAdapter.args).to.eql([[]]);
+
+      expect(utils.handlers.ErrorHandlerGenerator.args[0]).to.eql([services.metrics]);
+      expect(utils.interceptors.RequestInterceptorGenerator.args[0]).to.eql([services.metrics, services.adapter]);
+    });
   });
 
   describe('RequestInterceptor', () => {
