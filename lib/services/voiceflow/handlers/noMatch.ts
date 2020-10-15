30a7f4d7e98c9e7ffe7954717981c9591fc3550a
Josh <josh@voiceflow.com>
2020-10-14 20:30:52 -0400

    fix: remove empty reprompt speaks (CORE-4085)

--- lib/services/voiceflow/handlers/noMatch.ts
+++ lib/services/voiceflow/handlers/noMatch.ts
@@ -14,16 +14,25 @@ type NoMatchNode = Node<
   }
 >;
 
+const removeEmptyNoMatches = (noMatchArray?: string[]) => {
+  return noMatchArray?.filter((noMatch) => {
+    return noMatch != null;
+  });
+};
+
 export const NoMatchHandler = () => ({
   canHandle: (node: NoMatchNode, context: Context) => {
-    return Array.isArray(node.noMatches) && node.noMatches.length > (context.storage.get(S.NO_MATCHES_COUNTER) ?? 0);
+    const nonEmptyNoMatches = removeEmptyNoMatches(node.noMatches);
+    return Array.isArray(nonEmptyNoMatches) && nonEmptyNoMatches.length > (context.storage.get(S.NO_MATCHES_COUNTER) ?? 0);
   },
   handle: (node: NoMatchNode, context: Context, variables: Store) => {
     context.storage.produce((draft) => {
       draft[S.NO_MATCHES_COUNTER] = draft[S.NO_MATCHES_COUNTER] ? draft[S.NO_MATCHES_COUNTER] + 1 : 1;
     });
 
-    const speak = (node.randomize ? _.sample(node.noMatches) : node.noMatches?.[context.storage.get(S.NO_MATCHES_COUNTER) - 1]) || '';
+    const nonEmptyNoMatches = removeEmptyNoMatches(node.noMatches);
+
+    const speak = (node.randomize ? _.sample(nonEmptyNoMatches) : nonEmptyNoMatches?.[context.storage.get(S.NO_MATCHES_COUNTER) - 1]) || '';
 
     const sanitizedVars = sanitizeVariables(variables.getState());
     // replaces var values
