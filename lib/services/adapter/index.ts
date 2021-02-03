d4db124a103b5b88e5610b6f4d1b0cc947791192
leartgjoni-voiceflow <leart@voiceflow.com>
2021-02-03 10:29:23 -0500

    fix: logging format

--- lib/services/adapter/index.ts
+++ lib/services/adapter/index.ts
@@ -23,7 +23,7 @@ class AdapterManager extends AbstractManager {
       // set transformed context
       input.attributesManager.setPersistentAttributes(transformedContext);
 
-      log.warn('transformed context (skillId, userId)', versionID, input.requestEnvelope?.context?.System?.user?.userId);
+      log.warn('transformed context (skillId=%s, userId=%s)', versionID, input.requestEnvelope?.context?.System?.user?.userId);
     }
   }
 
@@ -43,7 +43,7 @@ class AdapterManager extends AbstractManager {
         variables,
       };
     } catch (err) {
-      log.error('context adapter err: ', err.message);
+      log.error('context adapter err: %s', err.message);
       return {};
     }
   }
