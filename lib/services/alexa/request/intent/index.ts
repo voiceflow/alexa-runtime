049f9d9db1a2fe21fa5cb4eb69b0e73a38b87f29
Tyler Han <tyler@voiceflow.com>
2021-03-05 00:22:09 -0500

    fix: more debugging logs

--- lib/services/alexa/request/intent/index.ts
+++ lib/services/alexa/request/intent/index.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'ask-sdk';
 
+import log from '@/logger';
+
 import { AlexaHandlerInput, Request } from '../../types';
 import { buildResponse, buildRuntime, initialize, update } from '../lifecycle';
 import behavior from './behavior';
@@ -25,6 +27,10 @@ export const IntentHandlerGenerator = (utils: typeof utilsObj): RequestHandler =
       await utils.initialize(runtime, input);
     }
 
+    if (runtime.versionID === '6041446c8f74b3001c175b5c') {
+      log.warn('BEFORE 6041446c8f74b3001c175b5c stack=%s', JSON.stringify(runtime.stack.getState()));
+    }
+
     // eslint-disable-next-line no-restricted-syntax
     for (const handler of utils.behavior) {
       if (handler.canHandle(input, runtime)) {
@@ -33,6 +39,10 @@ export const IntentHandlerGenerator = (utils: typeof utilsObj): RequestHandler =
     }
 
     await utils.update(runtime);
+
+    if (runtime.versionID === '6041446c8f74b3001c175b5c') {
+      log.warn('AFTER 6041446c8f74b3001c175b5c stack=%s', JSON.stringify(runtime.stack.getState()));
+    }
 
     return utils.buildResponse(runtime, input);
   },
