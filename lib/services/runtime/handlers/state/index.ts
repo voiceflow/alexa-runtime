b8cf0b984dfcf4aa275cd18eadc555229dccaab2
leartgjoni-voiceflow <leart@voiceflow.com>
2021-03-22 15:41:01 -0400

    feat: preliminary handler

--- lib/services/runtime/handlers/state/index.ts
+++ lib/services/runtime/handlers/state/index.ts
@@ -1,6 +1,7 @@
 import cancelPaymentStateHandler from './cancelPayment';
 import oneShotIntentHandler from './oneShotIntent';
 import paymentStateHandler from './payment';
+import preliminaryHandler from './preliminary';
 import streamStateHandler from './stream';
 
-export default () => [oneShotIntentHandler(), paymentStateHandler(), cancelPaymentStateHandler(), streamStateHandler()];
+export default () => [oneShotIntentHandler(), paymentStateHandler(), cancelPaymentStateHandler(), streamStateHandler(), preliminaryHandler()];
