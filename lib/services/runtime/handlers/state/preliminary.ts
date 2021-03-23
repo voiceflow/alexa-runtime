82baa447a55ea2de6a3db5125de2cda817190c69
leartgjoni-voiceflow <leart@voiceflow.com>
2021-03-23 11:57:42 -0400

    fix: dont match initial req

--- lib/services/runtime/handlers/state/preliminary.ts
+++ lib/services/runtime/handlers/state/preliminary.ts
@@ -21,16 +21,16 @@ const utilsObj = {
 export const PreliminaryHandler: HandlerFactory<Node<any, any>, typeof utilsObj> = (utils) => ({
   canHandle: (node, runtime, variables, program) => {
     const request = runtime.turn.get(T.REQUEST);
-    return !!request && !utils.eventHandlers.find((h) => h.canHandle(node, runtime, variables, program));
+    return !!request && !runtime.turn.get(T.NEW_STACK) && !utils.eventHandlers.find((h) => h.canHandle(node, runtime, variables, program));
   },
   handle: (node, runtime, variables) => {
-    // request for this turn has been processed, delete request
-    runtime.turn.delete(T.REQUEST);
-
     // check if there is a command in the stack that fulfills request
     if (utils.commandHandler.canHandle(runtime)) {
       return utils.commandHandler.handle(runtime, variables);
     }
+
+    // request for this turn has been processed, delete request
+    runtime.turn.delete(T.REQUEST);
 
     // return current id
     return node.id;
