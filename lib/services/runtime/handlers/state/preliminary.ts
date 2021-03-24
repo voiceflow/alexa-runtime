afd69f7049190997a2ee3b9a8bd385a061d389a1
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2021-03-24 12:27:08 -0400

    feat: preliminary handler (CORE-5458) (#143)
    * feat: preliminary handler
    
    * fix: dont match initial req
    
    * test: fix

---
+++ lib/services/runtime/handlers/state/preliminary.ts
@@ -0,0 +1,40 @@
+import { Node } from '@voiceflow/api-sdk';
+import { HandlerFactory } from '@voiceflow/runtime';
+
+import { T } from '@/lib/constants';
+
+import CaptureHandler from '../capture';
+import CommandHandler from '../command';
+import InteractionHandler from '../interaction';
+
+export const eventHandlers = [CaptureHandler(), InteractionHandler()];
+
+const utilsObj = {
+  commandHandler: CommandHandler(),
+  eventHandlers,
+};
+
+/**
+ * If request comes in but runtime nodeID is not a node that handles events (i.e, interaction, capture, etc..) =>
+ * Handle it here
+ */
+export const PreliminaryHandler: HandlerFactory<Node<any, any>, typeof utilsObj> = (utils) => ({
+  canHandle: (node, runtime, variables, program) => {
+    const request = runtime.turn.get(T.REQUEST);
+    return !!request && !runtime.turn.get(T.NEW_STACK) && !utils.eventHandlers.find((h) => h.canHandle(node, runtime, variables, program));
+  },
+  handle: (node, runtime, variables) => {
+    // check if there is a command in the stack that fulfills request
+    if (utils.commandHandler.canHandle(runtime)) {
+      return utils.commandHandler.handle(runtime, variables);
+    }
+
+    // request for this turn has been processed, delete request
+    runtime.turn.delete(T.REQUEST);
+
+    // return current id
+    return node.id;
+  },
+});
+
+export default () => PreliminaryHandler(utilsObj);
---
+++ tests/lib/services/voiceflow/handlers/state/preliminary.unit.ts
@@ -0,0 +1,87 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { T } from '@/lib/constants';
+import { PreliminaryHandler as PreliminaryHandlerFactory } from '@/lib/services/runtime/handlers/state/preliminary';
+
+describe('preliminary handler unit tests', () => {
+  describe('canHandle', () => {
+    it('not expected request', () => {
+      const runtime = { turn: { get: sinon.stub().returns(null) } };
+      expect(PreliminaryHandlerFactory({ eventHandlers: [] } as any).canHandle(null as any, runtime as any, null as any, null as any)).to.eql(false);
+    });
+
+    it('is new stack', () => {
+      const runtime = {
+        turn: {
+          get: sinon
+            .stub()
+            .onFirstCall()
+            .returns(null)
+            .onSecondCall()
+            .returns(true),
+        },
+      };
+      expect(PreliminaryHandlerFactory({ eventHandlers: [] } as any).canHandle(null as any, runtime as any, null as any, null as any)).to.eql(false);
+    });
+
+    it('handler found', () => {
+      const runtime = {
+        turn: {
+          get: sinon
+            .stub()
+            .onFirstCall()
+            .returns({ payload: { name: 'event1' } })
+            .onSecondCall()
+            .returns(null),
+        },
+      };
+      const eventHandlers = [{ canHandle: sinon.stub().returns(true) }];
+      expect(PreliminaryHandlerFactory({ eventHandlers } as any).canHandle(null as any, runtime as any, null as any, null as any)).to.eql(false);
+    });
+
+    it('true', () => {
+      const runtime = {
+        turn: {
+          get: sinon
+            .stub()
+            .onFirstCall()
+            .returns({ payload: { name: 'event1' } })
+            .onSecondCall()
+            .returns(null),
+        },
+      };
+      expect(PreliminaryHandlerFactory({ eventHandlers: [] } as any).canHandle(null as any, runtime as any, null as any, null as any)).to.eql(true);
+    });
+  });
+
+  describe('handle', () => {
+    it('command can handle', () => {
+      const nodeID = 'node-id';
+      const utils = {
+        commandHandler: { canHandle: sinon.stub().returns(true), handle: sinon.stub().returns(nodeID) },
+      };
+      const handler = PreliminaryHandlerFactory(utils as any);
+
+      const runtime = {};
+      const variables = { var1: 'val1', var2: 'val2' };
+      expect(handler.handle(null as any, runtime as any, variables as any, null as any)).to.eql(nodeID);
+      expect(utils.commandHandler.canHandle.args).to.eql([[runtime]]);
+      expect(utils.commandHandler.handle.args).to.eql([[runtime, variables]]);
+    });
+
+    it('command cant handle', () => {
+      const utils = {
+        commandHandler: { canHandle: sinon.stub().returns(false) },
+      };
+      const handler = PreliminaryHandlerFactory(utils as any);
+
+      const node = { id: 'id' };
+      const runtime = { turn: { delete: sinon.stub() } };
+      const variables = { var1: 'val1', var2: 'val2' };
+      expect(handler.handle(node as any, runtime as any, variables as any, null as any)).to.eql(node.id);
+      expect(runtime.turn.delete.args).to.eql([[T.REQUEST]]);
+      expect(utils.commandHandler.canHandle.args).to.eql([[runtime]]);
+    });
+  });
+});
