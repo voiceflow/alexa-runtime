8ad7b52bfac89c020282e3220d0e38a2f8f99082
Tyler Han <tyler@voiceflow.com>
2021-03-30 10:49:53 -0400

    fix: temporarily disable prelim handler

--- lib/services/runtime/handlers/state/index.ts
+++ lib/services/runtime/handlers/state/index.ts
@@ -1,7 +1,13 @@
 import cancelPaymentStateHandler from './cancelPayment';
 import oneShotIntentHandler from './oneShotIntent';
 import paymentStateHandler from './payment';
-import preliminaryHandler from './preliminary';
+// import preliminaryHandler from './preliminary';
 import streamStateHandler from './stream';
 
-export default () => [oneShotIntentHandler(), paymentStateHandler(), cancelPaymentStateHandler(), streamStateHandler(), preliminaryHandler()];
+export default () => [
+  oneShotIntentHandler(),
+  paymentStateHandler(),
+  cancelPaymentStateHandler(),
+  streamStateHandler(),
+  // preliminaryHandler()
+];
