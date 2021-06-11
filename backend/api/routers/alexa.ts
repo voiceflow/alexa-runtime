6ffe91298725a4ec8b321189c07f9f291d3396b9
Jonah Snider <jonah@jonahsnider.com>
2021-06-11 14:28:25 -0700

    feat: use `secure-json-parse` and `@voiceflow/bodyparser`

--- backend/api/routers/alexa.ts
+++ backend/api/routers/alexa.ts
@@ -1,5 +1,6 @@
-import bodyParser from 'body-parser';
+import bodyParser from '@voiceflow/body-parser';
 import express from 'express';
+import sjson from 'secure-json-parse';
 
 import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
 import { ControllerMap, MiddlewareMap } from '@/lib';
@@ -8,7 +9,7 @@ export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
   const router = express.Router();
 
   router.use(middlewares.alexa.verifier);
-  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT }));
+  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT, customJSONParser: sjson.parse }));
   router.post('/:versionID', controllers.alexa.handler);
 
   return router;
