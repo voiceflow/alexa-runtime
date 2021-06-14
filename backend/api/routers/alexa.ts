10f3633d6c21b54e1eda53d90ea1868e21dc9f6f
Jonah Snider <jonah@jonahsnider.com>
2021-06-14 13:56:20 -0700

    refactor: use default JSON parser function

--- backend/api/routers/alexa.ts
+++ backend/api/routers/alexa.ts
@@ -1,6 +1,5 @@
 import bodyParser from '@voiceflow/body-parser';
 import express from 'express';
-import sjson from 'secure-json-parse';
 
 import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
 import { ControllerMap, MiddlewareMap } from '@/lib';
@@ -9,7 +8,7 @@ export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
   const router = express.Router();
 
   router.use(middlewares.alexa.verifier);
-  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT, customJSONParser: sjson.parse }));
+  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT }));
   router.post('/:versionID', controllers.alexa.handler);
 
   return router;
