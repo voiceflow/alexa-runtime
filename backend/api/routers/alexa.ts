38e467dad94dc6249cc41f28ab6826ab96d633c3
Jonah Snider <jonah.snider@voiceflow.com>
2021-06-15 13:31:28 -0700

    feat: use `secure-json-parse` and `@voiceflow/body-parser` (VF-991) (#168)
    * feat: use `secure-json-parse` and `@voiceflow/bodyparser`
    
    * refactor: use default JSON parser function

--- backend/api/routers/alexa.ts
+++ backend/api/routers/alexa.ts
@@ -1,4 +1,4 @@
-import bodyParser from 'body-parser';
+import bodyParser from '@voiceflow/body-parser';
 import express from 'express';
 
 import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
