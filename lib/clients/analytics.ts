57017b36f7670b6de4b9b66c7fe3853ace46b40e
xavidop <xavi_tb@hotmail.com>
2021-07-22 11:55:30 +0200

    fix: commented rudderstack client

--- lib/clients/analytics.ts
+++ lib/clients/analytics.ts
@@ -1,4 +1,3 @@
-import Rudderstack, { IdentifyRequest, TrackRequest } from '@rudderstack/rudder-sdk-node';
 import { State } from '@voiceflow/general-runtime/build/runtime';
 import { Response } from 'ask-sdk-model';
 
@@ -10,46 +9,48 @@ import IngestApiClient, { Event, IngestApi, InteractBody, RequestType, TurnBody 
 import { AbstractClient } from './utils';
 
 export class AnalyticsSystem extends AbstractClient {
-  private rudderstackClient?: Rudderstack;
+  // Rudderstack client is commented due to a possible use in a near future
+  // private rudderstackClient?: Rudderstack;
 
   private ingestClient?: IngestApi;
 
-  private aggregateAnalytics = false;
+  // private aggregateAnalytics = false;
 
   constructor(config: Config) {
     super(config);
 
-    if (config.ANALYTICS_WRITE_KEY && config.ANALYTICS_ENDPOINT) {
-      this.rudderstackClient = new Rudderstack(config.ANALYTICS_WRITE_KEY, `${config.ANALYTICS_ENDPOINT}/v1/batch`);
-    }
+    // if (config.ANALYTICS_WRITE_KEY && config.ANALYTICS_ENDPOINT) {
+    //   this.rudderstackClient = new Rudderstack(config.ANALYTICS_WRITE_KEY, `${config.ANALYTICS_ENDPOINT}/v1/batch`);
+    // }
 
     if (config.INGEST_WEBHOOK_ENDPOINT) {
       this.ingestClient = IngestApiClient(config.INGEST_WEBHOOK_ENDPOINT, undefined);
     }
-    this.aggregateAnalytics = !config.IS_PRIVATE_CLOUD;
+    // this.aggregateAnalytics = !config.IS_PRIVATE_CLOUD;
   }
 
   identify(id: string) {
-    const payload: IdentifyRequest = {
-      userId: id,
-    };
+    log.trace(`analytics: Identify ${id}`);
 
-    if (this.aggregateAnalytics && this.rudderstackClient) {
-      log.trace('analytics: Identify');
-      this.rudderstackClient.identify(payload);
-    }
+    // const payload: IdentifyRequest = {
+    //   userId: id,
+    // };
+
+    // if (this.aggregateAnalytics && this.rudderstackClient) {
+    //   this.rudderstackClient.identify(payload);
+    // }
   }
 
-  private callAnalyticsSystemTrack(id: string, eventId: Event, metadata: InteractBody) {
-    const interactAnalyticsBody: TrackRequest = {
-      userId: id,
-      event: eventId,
-      properties: {
-        metadata,
-      },
-    };
-    this.rudderstackClient!.track(interactAnalyticsBody);
-  }
+  // private callAnalyticsSystemTrack(id: string, eventId: Event, metadata: InteractBody) {
+  //   const interactAnalyticsBody: TrackRequest = {
+  //     userId: id,
+  //     event: eventId,
+  //     properties: {
+  //       metadata,
+  //     },
+  //   };
+  //   this.rudderstackClient!.track(interactAnalyticsBody);
+  // }
 
   private createInteractBody({
     eventID,
@@ -130,18 +131,18 @@ export class AnalyticsSystem extends AbstractClient {
         const turnIngestBody = this.createTurnBody({ versionID: id, eventID: event, sessionID: sessionid, metadata, timestamp });
 
         // User/initial interact
-        if (this.aggregateAnalytics && this.rudderstackClient) {
-          this.callAnalyticsSystemTrack(id, event, turnIngestBody);
-        }
+        // if (this.aggregateAnalytics && this.rudderstackClient) {
+        //   this.callAnalyticsSystemTrack(id, event, turnIngestBody);
+        // }
         const turnResponse = await this.ingestClient?.doIngest(turnIngestBody);
         const turnID = turnResponse?.data.turn_id!;
 
         const interactIngestBody = this.createInteractBody({ eventID: Event.INTERACT, request, payload, turnID, timestamp });
 
         // User/initial interact
-        if (this.aggregateAnalytics && this.rudderstackClient) {
-          this.callAnalyticsSystemTrack(id, event, interactIngestBody);
-        }
+        // if (this.aggregateAnalytics && this.rudderstackClient) {
+        //   this.callAnalyticsSystemTrack(id, event, interactIngestBody);
+        // }
         await this.ingestClient?.doIngest(interactIngestBody);
 
         return turnID;
@@ -150,9 +151,9 @@ export class AnalyticsSystem extends AbstractClient {
         const interactIngestBody = this.createInteractBody({ eventID: event, request, payload, turnID: turnIDP!, timestamp });
 
         // User/initial interact
-        if (this.aggregateAnalytics && this.rudderstackClient) {
-          this.callAnalyticsSystemTrack(id, event, interactIngestBody);
-        }
+        // if (this.aggregateAnalytics && this.rudderstackClient) {
+        //   this.callAnalyticsSystemTrack(id, event, interactIngestBody);
+        // }
 
         await this.ingestClient?.doIngest(interactIngestBody);
         return turnIDP!;
--- tests/lib/clients/analytics.unit.ts
+++ tests/lib/clients/analytics.unit.ts
@@ -5,22 +5,22 @@ import AnalyticsClient from '@/lib/clients/analytics';
 import { Event, RequestType } from '@/lib/clients/ingest-client';
 
 describe('Analytics client unit tests', () => {
-  describe('Identify', () => {
-    it('works', () => {
-      const config = {};
+  // describe('Identify', () => {
+  //   it('works', () => {
+  //     const config = {};
 
-      const rudderstack = { identify: sinon.stub() };
+  //     const rudderstack = { identify: sinon.stub() };
 
-      const client = AnalyticsClient(config as any);
+  //     const client = AnalyticsClient(config as any);
 
-      (client as any).rudderstackClient = rudderstack;
+  //     (client as any).rudderstackClient = rudderstack;
 
-      client.identify('user id');
+  //     client.identify('user id');
 
-      expect(rudderstack.identify.callCount).to.eql(1);
-      expect(rudderstack.identify.getCall(0).args).to.deep.eq([{ userId: 'user id' }]);
-    });
-  });
+  //     expect(rudderstack.identify.callCount).to.eql(1);
+  //     expect(rudderstack.identify.getCall(0).args).to.deep.eq([{ userId: 'user id' }]);
+  //   });
+  // });
 
   describe('Track', () => {
     it('throws on unknown events', () => {
@@ -50,64 +50,64 @@ describe('Analytics client unit tests', () => {
       ).to.eventually.rejectedWith(RangeError);
     });
 
-    it('works with interact events', () => {
-      const config = {
-        ANALYTICS_WRITE_KEY: 'write key',
-        ANALYTICS_ENDPOINT: 'http://localhost/analytics',
-        INGEST_WEBHOOK_ENDPOINT: 'http://localhost/ingest',
-      };
+    // it('works with interact events', () => {
+    //   const config = {
+    //     ANALYTICS_WRITE_KEY: 'write key',
+    //     ANALYTICS_ENDPOINT: 'http://localhost/analytics',
+    //     INGEST_WEBHOOK_ENDPOINT: 'http://localhost/ingest',
+    //   };
 
-      const metadata = {
-        data: {
-          reqHeaders: {},
-          locale: 'locale',
-        },
-        stack: {},
-        storage: {},
-        variables: {},
-      };
+    //   const metadata = {
+    //     data: {
+    //       reqHeaders: {},
+    //       locale: 'locale',
+    //     },
+    //     stack: {},
+    //     storage: {},
+    //     variables: {},
+    //   };
 
-      const payload = {};
+    //   const payload = {};
 
-      const rudderstack = { track: sinon.stub() };
+    //   const rudderstack = { track: sinon.stub() };
 
-      const client = AnalyticsClient(config as any);
+    //   const client = AnalyticsClient(config as any);
 
-      (client as any).rudderstackClient = rudderstack;
+    //   (client as any).rudderstackClient = rudderstack;
 
-      const ingestClient = { doIngest: sinon.stub() };
+    //   const ingestClient = { doIngest: sinon.stub() };
 
-      (client as any).ingestClient = ingestClient;
-      const timestamp = new Date();
-      client.track({
-        id: 'id',
-        event: Event.INTERACT,
-        request: RequestType.REQUEST,
-        payload: payload as any,
-        sessionid: 'session.id',
-        metadata: metadata as any,
-        timestamp,
-      });
+    //   (client as any).ingestClient = ingestClient;
+    //   const timestamp = new Date();
+    //   client.track({
+    //     id: 'id',
+    //     event: Event.INTERACT,
+    //     request: RequestType.REQUEST,
+    //     payload: payload as any,
+    //     sessionid: 'session.id',
+    //     metadata: metadata as any,
+    //     timestamp,
+    //   });
 
-      expect(rudderstack.track.callCount).to.eql(1);
-      expect(rudderstack.track.getCall(0).args).to.deep.eq([
-        {
-          userId: 'id',
-          event: Event.INTERACT,
-          properties: {
-            metadata: {
-              eventId: Event.INTERACT,
-              request: {
-                type: RequestType.REQUEST,
-                payload: {},
-                format: 'request',
-                turn_id: undefined,
-                timestamp: timestamp.toISOString(),
-              },
-            },
-          },
-        },
-      ]);
-    });
+    //   expect(rudderstack.track.callCount).to.eql(1);
+    //   expect(rudderstack.track.getCall(0).args).to.deep.eq([
+    //     {
+    //       userId: 'id',
+    //       event: Event.INTERACT,
+    //       properties: {
+    //         metadata: {
+    //           eventId: Event.INTERACT,
+    //           request: {
+    //             type: RequestType.REQUEST,
+    //             payload: {},
+    //             format: 'request',
+    //             turn_id: undefined,
+    //             timestamp: timestamp.toISOString(),
+    //           },
+    //         },
+    //       },
+    //     },
+    //   ]);
+    // });
   });
 });
