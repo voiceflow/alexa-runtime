87ca6480fbdd4e9ab50ed975963d70201f71f709
Tyler Han <tyler@voiceflow.com>
2021-08-06 10:35:59 -0400

    feat: custom verifier (VF-000)

--- tests/lib/middlewares/alexa.unit.ts
+++ tests/lib/middlewares/alexa.unit.ts
@@ -1,8 +1,7 @@
-import AlexaVerifier from 'alexa-verifier-middleware';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import AlexaMiddleware from '@/lib/middlewares/alexa';
+import AlexaMiddleware, { AlexaVerifierMiddleware } from '@/lib/middlewares/alexa';
 
 describe('Alexa Middleware test', () => {
   describe('verifier', () => {
@@ -11,12 +10,12 @@ describe('Alexa Middleware test', () => {
       const { verifier } = new AlexaMiddleware(null as any, { NODE_ENV: 'test' } as any);
       verifier(null, null, next);
       expect(next.callCount).to.eql(1);
-      expect(verifier).to.not.eq(AlexaVerifier);
+      expect(verifier).to.not.eq(AlexaVerifierMiddleware);
     });
 
     it('normal env', () => {
       const { verifier } = new AlexaMiddleware(null as any, { NODE_ENV: 'not-test' } as any);
-      expect(verifier).to.eql(AlexaVerifier);
+      expect(verifier).to.eql(AlexaVerifierMiddleware);
     });
   });
 });
