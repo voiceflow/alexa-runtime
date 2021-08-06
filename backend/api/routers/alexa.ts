ae9a94dd5f1e72f40e1367313c4208a5dd525fe6
Tyler Han <tyler@voiceflow.com>
2021-08-06 16:04:23 -0400

    chore: remove unused

--- backend/api/routers/alexa.ts
+++ backend/api/routers/alexa.ts
@@ -1,7 +1,6 @@
 import bodyParser from '@voiceflow/body-parser';
 import express from 'express';
 
-import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
 import { ControllerMap, MiddlewareMap } from '@/lib';
 
 export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
