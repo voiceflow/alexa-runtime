bb97d4b02089e3aa4b1e77c4c770f187d9b71757
xavidop <xavi_tb@hotmail.com>
2021-08-09 12:20:18 +0200

    fix: fix alexa runtime when is called using the aduiplayer interface

--- config.ts
+++ config.ts
@@ -61,8 +61,7 @@ const CONFIG: Config = {
   ANALYTICS_ENDPOINT: getOptionalProcessEnv('ANALYTICS_ENDPOINT') || null,
   ANALYTICS_WRITE_KEY: getOptionalProcessEnv('ANALYTICS_WRITE_KEY') || null,
 
-  // temporarily turn off (too many errors)
-  INGEST_WEBHOOK_ENDPOINT: null, // getOptionalProcessEnv('INGEST_WEBHOOK_ENDPOINT') || null,
+  INGEST_WEBHOOK_ENDPOINT: getOptionalProcessEnv('INGEST_WEBHOOK_ENDPOINT') || null,
 };
 
 export default CONFIG;
