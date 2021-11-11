53e2758cbdbb9403c0c418eafdff40102a881dbd
leartgjoni-voiceflow <leart@voiceflow.com>
2021-11-11 10:37:36 -0500

    chore: revert response unit changes

--- tests/lib/services/alexa/request/lifecycle/response.unit.ts
+++ tests/lib/services/alexa/request/lifecycle/response.unit.ts
@@ -18,8 +18,7 @@ describe('response lifecycle unit tests', () => {
     const storageGet = sinon.stub().returns('speak');
     const turnGet = sinon.stub();
     turnGet.onFirstCall().returns(null);
-    turnGet.onSecondCall().returns(null);
-    turnGet.onThirdCall().returns(true);
+    turnGet.onSecondCall().returns(true);
     const versionID = 'version.id';
     const turnID = 'turn-id';
     const request = { foo: 'bar' };
@@ -80,94 +79,6 @@ describe('response lifecycle unit tests', () => {
         },
       ],
     ]);
-    expect(input.responseBuilder.speak.args).to.eql([['speak']]);
-    expect(reprompt.args).to.eql([['speak']]);
-    expect(withShouldEndSession.args).to.eql([[true]]);
-    expect(responseHandler1.args).to.eql([[runtime, input.responseBuilder]]);
-    expect(responseHandler2.args).to.eql([[runtime, input.responseBuilder]]);
-    expect(input.attributesManager.setPersistentAttributes.args).to.eql([[finalState]]);
-  });
-
-  it('goto', async () => {
-    const responseHandler1 = sinon.stub();
-    const responseHandler2 = sinon.stub();
-
-    const utils = { responseHandlers: [responseHandler1, responseHandler2] };
-
-    const response = responseGenerator(utils);
-
-    const finalState = 'final-state';
-    const storageGet = sinon.stub().returns('speak');
-    const goToIntent = 'go-to-intent';
-    const turnGet = sinon.stub();
-    turnGet.onFirstCall().returns(goToIntent);
-    turnGet.onSecondCall().returns(null);
-    turnGet.onThirdCall().returns(true);
-    const versionID = 'version.id';
-    const turnID = 'turn-id';
-    const request = { foo: 'bar' };
-    const runtime = {
-      getRequest: sinon.stub().returns(request),
-      storage: { get: storageGet },
-      turn: { get: turnGet, set: sinon.stub() },
-      stack: { isEmpty: sinon.stub().returns(true) },
-      variables: { get: sinon.stub().returns(false) },
-      getFinalState: sinon.stub().returns(finalState),
-      services: {
-        analyticsClient: {
-          track: sinon.stub().resolves(turnID),
-        },
-      },
-      getVersionID: sinon.stub().returns(versionID),
-    };
-    const accessToken = 'access-token';
-    const output = 'output';
-
-    const withShouldEndSession = sinon.stub();
-    const reprompt = sinon.stub().returns({ withShouldEndSession });
-    const input = {
-      responseBuilder: {
-        getResponse: sinon.stub().returns(output),
-        speak: sinon.stub().returns({ reprompt }),
-        addDelegateDirective: sinon.stub(),
-      },
-      requestEnvelope: {
-        runtime: { System: { user: { accessToken } } },
-        session: { sessionId: 'session.id' },
-      },
-      attributesManager: { setPersistentAttributes: sinon.stub() },
-    };
-
-    expect(await response(runtime as any, input as any)).to.eql(output);
-    const [[{ timestamp }], [{ timestamp: timestamp2 }]] = runtime.services.analyticsClient.track.args;
-    expect(runtime.turn.set.args).to.eql([[T.END, true]]);
-    expect(runtime.storage.get.args).to.eql([[S.OUTPUT], [S.OUTPUT]]);
-    expect(runtime.services.analyticsClient.track.args).to.eql([
-      [
-        {
-          id: versionID,
-          event: Ingest.Event.TURN,
-          request: Ingest.RequestType.REQUEST,
-          payload: request,
-          sessionid: input.requestEnvelope.session.sessionId,
-          metadata: finalState,
-          timestamp,
-        },
-      ],
-      [
-        {
-          id: versionID,
-          event: Ingest.Event.INTERACT,
-          request: Ingest.RequestType.RESPONSE,
-          payload: output,
-          sessionid: input.requestEnvelope.session.sessionId,
-          metadata: finalState,
-          timestamp: timestamp2,
-          turnIDP: turnID,
-        },
-      ],
-    ]);
-    expect(input.responseBuilder.addDelegateDirective.args).to.eql([[{ name: goToIntent, confirmationStatus: 'NONE', slots: {} }]]);
     expect(input.responseBuilder.speak.args).to.eql([['speak']]);
     expect(reprompt.args).to.eql([['speak']]);
     expect(withShouldEndSession.args).to.eql([[true]]);
