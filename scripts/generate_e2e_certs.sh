76ed2fec744b40f7a8efe88f01d9a5c6fa31180b
Tyler Han <tylerhan97@gmail.com>
2021-12-07 15:37:46 -0500

    chore: add e2e endpoint to cert gen (VF-000)

--- scripts/generate_e2e_certs.sh
+++ scripts/generate_e2e_certs.sh
@@ -3,7 +3,7 @@
 rm -rf certs/
 mkdir -p certs/ && cd certs/
 LAN_IP=$(ifconfig en0 | grep "inet " | awk '{print $2}')
-mkcert localhost 127.0.0.1 ::1 $(echo "$(hostname)" | tr '[:upper:]' '[:lower:]') $(hostname) $LAN_IP 192.168.18.1 0.0.0.0
+mkcert localhost 127.0.0.1 ::1 $(echo "$(hostname)" | tr '[:upper:]' '[:lower:]') $(hostname) $LAN_IP 192.168.18.1 0.0.0.0 *.test.e2e;
 
 # Rename certificate and key
 mv *-key.pem localhost.key
