44f6290ed1035c984b111290ec11f1dbe8ce598f
Tyler Han <tylerhan97@gmail.com>
2022-01-14 14:20:42 -0500

    fix: debug log (VF-000) (#255)

--- lib/controllers/alexa.ts
+++ lib/controllers/alexa.ts
@@ -1,6 +1,7 @@
 import { Validator } from '@voiceflow/backend-utils';
 
 import { AlexaContext } from '@/lib/services/alexa/types';
+import log from '@/logger';
 import { Request } from '@/types';
 
 import { validate } from '../utils';
@@ -19,6 +20,7 @@ class AlexaController extends AbstractController {
 
   @validate({ VERSION_ID: AlexaController.VALIDATIONS.PARAMS.versionID })
   async handler(req: Request<{ versionID: string }>) {
+    log.info(`handling skill ${req.params.versionID}`);
     const { alexa, runtimeClient, metrics, dataAPI } = this.services;
 
     metrics.request();
