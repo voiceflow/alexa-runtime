db07f29294831c3c0731875cbd084a3459adc70e
pmvrmc <pedro.matosdecarvalho@voiceflow.com>
2023-05-30 13:12:59 +0000

    feat:  mimic state/skill for infra redirect (PL-30) (#331)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-30**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -10,5 +10,9 @@ export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
   router.get('/health', (_, res) => res.send(`${process.env.NODE_ENV} Healthy`));
   router.use('/state/skill', AlexaRouter(middlewares, controllers));
 
+  // Remove this once the alexa-runtime is no longer being used for the redirect
+  // to the adapter/general-runtime
+  router.use('/state/skill-internal', AlexaRouter(middlewares, controllers));
+
   return router;
 };
