ea90bbfcbc6d27ff664bff326e7753c94a6e537d
Tyler Han <tylerhan97@gmail.com>
2024-04-02 10:57:32 -0400

    fix: add comments

--- lib/services/runtime/handlers/integrations.ts
+++ lib/services/runtime/handlers/integrations.ts
@@ -6,6 +6,8 @@ const VALID_INTEGRATIONS = new Set([
   BaseNode.Utils.IntegrationType.GOOGLE_SHEETS,
 ]);
 
+// the integrations node for zapier and google sheets is no longer supported
+// this is a stub handler to mitigate changes to agent behavior
 const IntegrationsHandler: HandlerFactory<BaseNode.Integration.Node> = () => ({
   canHandle: (node) =>
     node.type === BaseNode.NodeType.INTEGRATIONS && VALID_INTEGRATIONS.has(node.selected_integration),
